b31d68fd2d9c173d7c38d107a6a5d7338dc16383
Mateusz Perlik <mp429581@students.mimuw.edu.pl>
2024-01-01 23:37:15 +0100

    Multiple dirs in tester

--- tests/tester.py
+++ tests/tester.py
@@ -3,29 +3,32 @@ import os
 import subprocess
 from tqdm import tqdm
 
-# latte_dir = "tests/lattests/good"
-# latte_dir = "tests/lattests/extensions/arrays1"
-latte_dir = "tests/lattests/extensions/struct"
-# latte_dir = "tests/mrjp-tests-good/good"
-# latte_dir = "tests/mrjp-tests-benek/gr5"
 test_latte = "./latc_llvm"
+latte_dirs = [
+    "tests/lattests/good",
+    "tests/lattests/extensions/arrays1",
+    "tests/lattests/extensions/struct",
+    # "tests/mrjp-tests-good/good",
+    # "tests/mrjp-tests-benek/gr5"
+]
 
 errs = 0
 all = 0
-files = sorted([f for f in os.listdir(latte_dir) if f.endswith(".lat")])
+files = [f"{d}/{f}" for d in latte_dirs for f in os.listdir(d) if f.endswith(".lat")]
+files = sorted(files)
 for file in tqdm(files):
-    result = subprocess.run([test_latte, os.path.join(latte_dir, file)], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    result = subprocess.run([test_latte, file], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
     if result.returncode != 0:
         print(f"Error processing {file}")
         errs += 1
     else:
         # if exists file with .input extension, use it as input
-        if os.path.exists(os.path.join(latte_dir, file.replace(".lat", ".input"))):
-            with open(os.path.join(latte_dir, file.replace(".lat", ".input")), "r") as f:
+        if os.path.exists(file.replace(".lat", ".input")):
+            with open(file.replace(".lat", ".input"), "r") as f:
                 result = subprocess.run(["./out.bc"], stdin=f, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         else:
             result = subprocess.run(["./out.bc"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        with open(os.path.join(latte_dir, file.replace(".lat", ".output")), "r") as f:
+        with open(file.replace(".lat", ".output"), "r") as f:
             expected = f.read()
         if result.stdout.decode("utf-8") != expected:
             print(f"Error processing {file}")
